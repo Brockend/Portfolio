3cf867c4d58ff8a8f916c72f8074efe3b73870cb
Illia Kosyi <kosyj.illya.v@hneu.net>
2023-01-28 23:09:40 +0200

    Nice

--- js/script.js
+++ js/script.js
@@ -10,32 +10,3 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('header__hamburger_active');
     document.querySelector('.body').classList.toggle('body_active');
 });
-
-const form = document.querySelector(".contact__form");
-const responseContainer = document.getElementById("response");
-
-// Add an event listener to the form
-form.addEventListener("submit", function(e) {
-  // Prevent the form from submitting
-  e.preventDefault();
-
-  // Create a new FormData object
-  const formData = new FormData(form);
-
-  // Create a new XMLHttpRequest object
-  const xhr = new XMLHttpRequest();
-
-  // Open a connection to the server
-  xhr.open("POST", "sendmail.php", true);
-
-  // Send the form data
-  xhr.send(formData);
-
-  // Handle the response
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      // Update the response container
-      responseContainer.innerHTML = xhr.responseText;
-    }
-  };
-});
